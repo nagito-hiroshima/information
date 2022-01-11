953153a4bf1323a27e2607f900f759c6d72b3b49
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2022-01-11 23:42:33 +0900

    Update re.js

--- js/re.js
+++ js/re.js
@@ -2,7 +2,7 @@ function isSmartPhone() {
     if (screen.height == 800 && screen.width == 1280) {
         return
     } else {
-        location.href = "/notsupport";
+        location.href = "https://nagito.work/info/notsupport";
 
     }
 }
